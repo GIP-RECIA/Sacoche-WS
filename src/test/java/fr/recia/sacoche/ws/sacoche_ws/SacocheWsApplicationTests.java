a0fc8c0eafc87dcd46a62304afb172455564a5da
Guilment Claire <claire.guilment@recia.com>
2026-09-04 17:30:25 +0200

    feat: import initial spring boot skeleton

---
+++ src/test/java/fr/recia/sacoche/ws/sacoche_ws/SacocheWsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.recia.sacoche.ws.sacoche_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacocheWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
