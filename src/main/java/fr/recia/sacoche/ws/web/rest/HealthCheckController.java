b0f8c3df50c66bacf484c12c0acab12ef5980470
Guilment Claire <claire.guilment@recia.com>
2026-09-08 12:05:42 +0200

    feat: add unauthenticated health-check endpoint

---
+++ src/main/java/fr/recia/sacoche/ws/web/rest/HealthCheckController.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.sacoche.ws.web.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class HealthCheckController {
+
+    @GetMapping(value = "/health-check")
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck(HttpServletRequest request, HttpServletResponse response) {
+        log.debug("Doing a health check. Returning HTTP 200, OK");
+    }
+}
